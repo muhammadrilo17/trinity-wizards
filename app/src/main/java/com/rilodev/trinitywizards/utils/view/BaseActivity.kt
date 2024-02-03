be4f8ffa65f00195acf3e42a3e31152974ff8a48
muhammadrilo17 <rilo.pambudi1998@gmail.com>
2024-02-03 11:29:44 +0700

    end commit

--- app/src/main/java/com/rilodev/trinitywizards/utils/view/BaseActivity.kt
+++ app/src/main/java/com/rilodev/trinitywizards/utils/view/BaseActivity.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.rilodev.trinitywizards.utils.CustomDialog
-import dagger.hilt.android.AndroidEntryPoint
 import kotlin.reflect.KClass
 
 abstract class BaseActivity<VB: ViewBinding, VM : ViewModel>(
